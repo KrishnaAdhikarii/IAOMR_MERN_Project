4a4bffc8eb3598f9a566bea4e9c8c29d5d9bcb7c
Krishna Adhikari <krishnaadhikari0213@gmail.com>
2026-06-22 19:18:29 +0530

    disable

--- iaomr/frontend/src/components/SubmissionPage.jsx
+++ iaomr/frontend/src/components/SubmissionPage.jsx
@@ -473,7 +473,7 @@ export function SubmitPPTPage() {
             <button
             disabled
               onClick={() => window.open("/templates/ppt-template.pptx", "_blank")}
-              className="bg-white text-[rgb(27,46,87)] px-6 py-3 rounded-2xl font-semibold shadow-md hover:bg-blue-50 cursor-not-allowed opacity-60 transition whitespace-nowrap"
+              className="bg-white text-[rgb(27,46,87)] px-6 py-3 rounded-2xl font-semibold shadow-md hover:bg-blue-50 transition whitespace-nowrap"
             >
               📥 Download Template
             </button>
